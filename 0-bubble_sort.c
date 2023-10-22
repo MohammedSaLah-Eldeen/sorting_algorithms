c373d816ed3c191edd6716790597166afa95aa6d
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-10-22 20:19:32 +0000

    Bubble Sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,32 +1,33 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sort array elements in an ascending order
- * @array: array
- * @size: array size
- *
- */
+* bubble_sort - sort array elements in an ascending order
+* @array: array
+* @size: array size
+*
+*/
 void bubble_sort(int *array, size_t size)
 {
-  int i, s = size;
-  int flipped = 1;
-  int tmp;
-  
-  if (size < 2) return;
-  
-  while (flipped)
-    {
-      flipped = 0;
-      for (i = 0; i < s; i++)
-	{
-	 if (array[i + 1] < array[i])
-	   {
-	    tmp = array[i];
-	    array[i] = array[i + 1];
-	    array[i + 1] = tmp;
-	    print_array(array, size);
-	    flipped = 1;
-	   }
-	}
-    }
+int i, s = size;
+int flipped = 1;
+int tmp;
+
+if (size < 2 || array == NULL)
+return;
+
+while (flipped)
+{
+flipped = 0;
+for (i = 0; i < s; i++)
+{
+if (array[i + 1] < array[i])
+{
+tmp = array[i];
+array[i] = array[i + 1];
+array[i + 1] = tmp;
+print_array(array, size);
+flipped = 1;
+}
+}
+}
 }
